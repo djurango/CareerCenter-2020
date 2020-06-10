16944e899d8d2c04b96e2eea26448df05f433ca1
djurango <djuric@me.com>
2020-06-10 14:58:44 +0200

    removed filter

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Grid from "@material-ui/core/Grid/Grid";
 import Paper from "@material-ui/core/Paper/Paper";
 import TextField from "@material-ui/core/TextField";
-import Autocomplete from '@material-ui/lab/Autocomplete';
 
 
 class JobList extends Component {
@@ -10,10 +9,9 @@ class JobList extends Component {
     state = {
         isLoading: true,
         jobs: [],
+        apiJobs: [],
         error: null
     }
-
-    apiJobs = [];
 
     fetchJobs() {
         fetch('https://ohws.prospective.ch/public/v1/medium/1000470/jobs?lang=de&limit=200&offset=0')
@@ -45,48 +43,16 @@ class JobList extends Component {
 
     render() {
         const {isLoading, jobs, error} = this.state;
-
-        const top100Films = [
-            { title: "Finanzen/Controlling"},
-            { title: "Engineering"},
-            { title: "Qualitätsmanagement"},
-            { title: "Einkauf/Beschaffung"},
-            { title: "Informatik"},
-            { title: "Service/After Market Service"},
-            { title: "Human Resources"},
-        ];
-
         return (
             <main>
                 <h1>Jobs</h1>
 
                 <Grid className={"filterContainer"} container spacing={2}>
-
-                    <Grid item xs={12} sm={6} >
+                    <Grid item xs={12} sm={12} >
                         <TextField variant="outlined" id="standard-basic" label="Suchbegriff" type="text" value={this.state.value} onChange={this.onChangeHandler.bind(this)}/>
-                    </Grid>
-
-                    <Grid item xs={12} sm={6} >
-                        <Autocomplete
-                            multiple
-                            id="tags-outlined"
-                            options={top100Films}
-                            getOptionLabel={(option) => option.title}
-                            filterSelectedOptions
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    variant="outlined"
-                                    label="Abteilung"
-                                />
-                            )}
-                        />
                     </Grid>
                     {error ? <p>Keine Jobs, sorry</p> : null}
                 </Grid>
-
-
-
 
                 <Grid container spacing={2}>
                     {!isLoading ? (
@@ -112,17 +78,9 @@ class JobList extends Component {
                         </div>
                     )}
                 </Grid>
-
-
-
-
-
-
-
             </main>
         );
     }
-
 }
 
 export default JobList;
