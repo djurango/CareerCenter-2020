b6ca73ca70e8d449659a4f80a76e4ab6cd2ffead
djurango <djuric@me.com>
2020-06-10 16:25:07 +0200

    start working on filters

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -5,7 +5,6 @@ import TextField from "@material-ui/core/TextField";
 import loadingImg from '../assets/loading.gif';
 
 
-
 class JobList extends Component {
 
     state = {
@@ -19,7 +18,7 @@ class JobList extends Component {
     fetchJobs() {
         fetch('https://ohws.prospective.ch/public/v1/medium/1000470/jobs?lang=de&limit=200&offset=0')
             .then(response => response.json())
-            .then(data =>{
+            .then(data => {
                     this.apiJobs = data.jobs;
                     this.setState({
                         jobs: data.jobs,
@@ -28,34 +27,83 @@ class JobList extends Component {
                     })
                 }
             )
-            .catch(error => this.setState({ error, isLoading: false }));
+            .catch(error => this.setState({error, isLoading: false}));
+    }
+
+    fetchFilters() {
+        fetch('https://ohws.prospective.ch/public/v1/medium/1000470/attributes/?lang=de')
+            .then(response => response.json())
+            .then(data => {
+                    this.setState({
+                        filters: data.attributes,
+                        isLoading: false,
+                    })
+                }
+            )
+            .catch(error => this.setState({error, isLoading: false}));
     }
 
     componentDidMount() {
         this.fetchJobs();
+        this.fetchFilters();
+
     }
 
     onChangeHandler(e) {
-        let newArray = this.apiJobs.filter((d)=>{
+        let newArray = this.apiJobs.filter((d) => {
             let searchValue = d.title.toLowerCase();
             return searchValue.indexOf(e.target.value) !== -1;
         });
         this.setState({
-            jobs:newArray
+            jobs: newArray
         })
     }
 
     render() {
-        const {isLoading, jobs, error, totalJobs} = this.state;
+        const {isLoading, jobs, error, totalJobs, filters} = this.state;
         return (
+
             <main>
+
+
+
+
                 <h1>Jobs</h1>
                 <h5>Zur Zeit sind {totalJobs.total} Stellen ausgeschrieben</h5>
 
 
+                <header>
+
+                    <div>
+                        {!isLoading ? (
+                            filters.map(filter => {
+                                return (
+                                    <div key={filter.id}>
+                                        <span>{filter.id} </span>
+                                        <span>{filter.name}</span>
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <div className={"loading"}>
+                                <img className={"loadingSpinner"} src={loadingImg} alt=""/>
+                                <div><b>Loading...</b></div>
+                            </div>
+                        )}
+
+                    </div>
+
+                </header>
+
+
+
+
+
+
                 <Grid className={"filterContainer"} container spacing={2}>
-                    <Grid item xs={12} sm={12} >
-                        <TextField variant="outlined" id="standard-basic" label="Suchbegriff" type="text" value={this.state.value} onChange={this.onChangeHandler.bind(this)}/>
+                    <Grid item xs={12} sm={12}>
+                        <TextField variant="outlined" id="standard-basic" label="Suchbegriff" type="text"
+                                   value={this.state.value} onChange={this.onChangeHandler.bind(this)}/>
                     </Grid>
                     {error ? <p>Keine Jobs, sorry</p> : null}
                 </Grid>
@@ -66,7 +114,7 @@ class JobList extends Component {
                             return (
                                 <Grid item xs={12} sm={12} key={job.id}>
                                     <a target="_blank" rel="noopener noreferrer" href={job.links.directlink}>
-                                        <Paper className={"element"} >
+                                        <Paper className={"element"}>
                                             <h2>{job.title}</h2>
                                             <br/>
                                             <div><b>Abteilung: </b>{job.attributes[10]}</div>
